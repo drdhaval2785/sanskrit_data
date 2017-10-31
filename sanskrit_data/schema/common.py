55e0ba3463937217b3cf9b3285df91033c19441f
vishvAsaH <vishvas.vasuki@gmail.com>
2017-10-31 16:59:26 -0700

    Better error handling.

--- sanskrit_data/schema/common.py
+++ sanskrit_data/schema/common.py
@@ -288,14 +288,14 @@ class JsonObject(object):
       logging.error("Exception message: " + e.message)
       logging.error("Schema is: " + jsonpickle.dumps(self.schema))
       logging.error("Context is: " + str(e.context))
-      logging.error("Best match is: " + best_match(errors=[e]))
+      logging.error("Best match is: " + str(best_match(errors=[e])))
       raise e
     except ValidationError as e:
       logging.error("Exception message: " + e.message)
       logging.error("self is: " + self)
       logging.error("Schema is: " + jsonpickle.dumps(self.schema))
       logging.error("Context is: " + str(e.context))
-      logging.error("Best match is: " + best_match(errors=[e]))
+      logging.error("Best match is: " + str(best_match(errors=[e])))
       logging.error("json_map is: " + json_map)
       raise e
 
