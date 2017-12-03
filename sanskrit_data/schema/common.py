384632db2d0de84fee9b321b3bf4a2cb9e646cab
vishvAsaH <vishvas.vasuki@gmail.com>
2017-12-03 12:57:25 -0800

    Better json decoding

--- sanskrit_data/schema/common.py
+++ sanskrit_data/schema/common.py
@@ -136,7 +136,7 @@ class JsonObject(object):
 
   @classmethod
   def make_from_pickledstring(cls, pickle):
-    if pickle.strip().startswith("["):
+    if str(pickle).strip().startswith("["):
       return cls.make_from_dict_list(jsonpickle.decode(pickle))
     else:
       obj = cls.make_from_dict(jsonpickle.decode(pickle))
